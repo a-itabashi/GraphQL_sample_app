16b6661af25e08412548f301b6a33aa863b4db23
Aguru itabashi <itabashi.a@gmail.com>
2020-01-29 23:44:43 +0900

    feat: index.vue

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,8 @@
+module Types
+  class QueryType < Types::BaseObject
+    field :all_posts, [PostType], null: false
+    def all_posts()
+      Post.all
+    end
+  end
+end
---
+++ app/javascript/packs/components/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul v-for="post in allPosts">
+      <li>タイトル: {{post.title}}</li>
+      <li>内容: {{post.content}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import gql from 'graphql-tag'
+  const POST_QUERY = gql`
+  query {
+    allPosts{
+      title
+      content
+    }
+  }`
+
+  export default {
+    name: 'index',
+    data() {
+      return {
+        allPosts: {}
+      }
+    },
+    apollo: {
+      allPosts: {
+        query:POST_QUERY
+      }
+    }
+  }
+</script>
